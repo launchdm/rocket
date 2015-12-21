fc197180f8ca859dd626031cc27db2db1761986c
tim <tim@launchdm.com>
2015-12-21 11:45:14 -0500

    move files from lib directory to includes directory

--- lib/image-sizes.php
+++
@@ -1,2 +0,0 @@
-<?php
-    add_image_size( 'bsg-featured-image', 1170, 630, true );
--- lib/sidebar-secondary-move-inside-content-sidebar-wrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
-add_action(    'genesis_after_content',              'genesis_get_sidebar_alt' );
