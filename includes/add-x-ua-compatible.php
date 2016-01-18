859a5a7c5d8654d5ab3c8528b3ce88c89b94aab9
tbcorr <tbcorr@gmail.com>
2016-01-18 11:58:34 -0500

    refactor code style

--- includes/add-x-ua-compatible.php
+++ includes/add-x-ua-compatible.php
@@ -2,9 +2,9 @@
 
 // Add X-UA-Compatible tag
 add_action( 'genesis_meta', 'bsg_x_ua_compatible' );
+function bsg_x_ua_compatible(){
 
-function bsg_x_ua_compatible() {
     ?>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<?php
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php
 }
